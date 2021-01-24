bfaa5965794de7b017957a1414e29243f9cbd0b6
Daniel Moon <dm32@illinois.edu>
2021-01-24 10:47:16 -0500

    add teacher-info page

--- public/urls.py
+++ public/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='sail-home'),
     path('about/', views.about, name='sail-about'),
     path('staff/', views.staff, name='sail-staff'),
+    path('teachers/', views.teachers, name='sail-teacher'),
 ]
